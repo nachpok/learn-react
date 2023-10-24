58153a3c649a688e0ca1ff633a0a23ec3aee8e14
nachlielEasy <nachliel@easyappshq.com>
2023-10-24 12:36:21 +0300

    Update PScale.ts

--- learn-react/src/ToDo/PScale.ts
+++ learn-react/src/ToDo/PScale.ts
@@ -1,10 +1,13 @@
+import { toJS } from "mobx";
 import { Todo } from "./List";
 export const api_domain =
   process.env.REACT_APP_NEXT_PUBLIC_TODO_API_URL ||
   process.env.NEXT_PUBLIC_TODO_API_URL;
 export async function toggleTodo(todo: Todo) {
   validateTodoProperties("toggleTodo", todo);
+  console.log("PScale.toggle.todo: ", toJS(todo));
   try {
+    console.log("PScale.todo.usersId: ", todo.userId);
     const url = `https://todo-api-juzg.vercel.app/api/todos/${todo.userId}/${todo.id}/toggle`;
     console.log("PScale fetch url: ", url);
     const res = await fetch(url);
@@ -23,12 +26,17 @@ export async function deleteTodo(todo: Todo) {
   }
 }
 export async function createTodo(todo: Todo) {
-  if (todo.userId && todo.id && todo.title) {
+  console.log("PScale.todo.usersId: ", todo.userId);
+  const url = `https://todo-api-juzg.vercel.app/api/todos/${todo.userId}/${todo.id}/toggle`;
+  try {
     const res = await fetch(
       `${api_domain}/api/todos/${todo.userId}/${todo.id}/${todo.title}/createTodo`
     );
+  } catch (error) {
+    throw Error(`Missing data,can't create todo. error: ${error}`);
+  }
+  if (todo.userId && todo.id && todo.title) {
   } else {
-    throw Error(`Missing data,can't create todo: ${JSON.stringify(todo)}`);
   }
 }
 export async function deleteAllTodos(userId: string) {
